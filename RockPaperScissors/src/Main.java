d0b2f0c6d7ce7dbb76ced3c82bfd4dabb2a33074
MeMeow <snooze92@mail.ru>
2023-09-23 13:03:52 +0500

    realize console game

--- RockPaperScissors/src/Main.java
+++ RockPaperScissors/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final String [] choices = {"Камень", "Ножницы", "Бумага"};
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int computerChoice = random.nextInt(choices.length);
+
+        System.out.println("Начало игры");
+        System.out.println("0. Камень\n1. Ножницы\n2. Бумага\n");
+        int playerChoice = scanner.nextInt();
+        System.out.println(computerChoice);
+        if (computerChoice == playerChoice) {
+            System.out.println("Ничья");
+        } else if (playerChoice == 0 && computerChoice == 1 || playerChoice == 1 && computerChoice == 2 || playerChoice == 2 && computerChoice == 0) {
+            System.out.println("Победил игрок");
+        } else {
+            System.out.println("Победил компьютер");
+        }
+
+
     }
 }
