4cb15693e8b3bd211e6ed894a253da34ea4aa124
MeMeow <snooze92@mail.ru>
2023-09-23 11:46:22 +0500

    add Cosines Servlet

---
+++ CosinesServlet/src/main/java/org/top/cosinesservlet/CosinesServlet.java
@@ -0,0 +1,37 @@
+package org.top.cosinesservlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "cosinesServlet", value = "/cos")
+public class CosinesServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        try {
+            double a = Double.parseDouble(request.getParameter("a"));
+            double b = Double.parseDouble(request.getParameter("b"));
+            double corner = Double.parseDouble(request.getParameter("corner"));
+            String type = request.getParameter("type");
+            double c;
+
+            if (type.contains("гр")) {
+                c = Math.sqrt(a * a + b * b - 2 * a * b * Math.cos(Math.toRadians(corner)));
+                out.println("Третья сторона = " + c);
+            } else if (type.contains("рад")) {
+                c = Math.sqrt(a * a + b * b - 2 * a * b * Math.cos(corner));
+                out.println("Третья сторона = " + c);
+            } else {
+                out.println("Неверный тип угла");
+            }
+        } catch (Exception e) {
+            out.println("Что-то пошло не так - " + e.getMessage());
+        }
+    }
+}
