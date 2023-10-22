0ca7f7dc413ce4732f6e9ea69c40898a3c4608a2
amberk1t <snooze92@mail.ru>
2023-10-22 13:38:56 +0500

    init total without additional

--- Spring/ProductsAndOrdersApiApp+Client/src/main/java/org/top/productsandordersapiapp/rdb/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package org.top.productsandordersapiapp.rdb;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import org.top.productsandordersapiapp.entity.Product;
-
-@Repository
-public interface ProductRepository extends CrudRepository<Product, Integer> {
-}
